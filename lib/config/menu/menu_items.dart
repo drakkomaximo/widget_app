6086f1e1ad487cf8910b351d57cff3ae7acf36a1
drakkomaximo <drakkomaximo1234@gmail.com>
2023-07-17 18:53:33 -0500

    Home Screen and App Theme have been created

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem(
+      {required this.title,
+      required this.subtitle,
+      required this.link,
+      required this.icon});
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+      icon: Icons.smart_button_outlined,
+      link: '/buttons',
+      subtitle: 'Varios botones en Fluter',
+      title: 'Botones'),
+  MenuItem(
+      icon: Icons.credit_card,
+      link: '/card',
+      subtitle: 'Un contenedor estilizado',
+      title: 'Tarjetas'),
+];
